47a1067aec175aeb300ad613f5a0fd76c2bd8957
danahamdanx <145384079+danahamdanx@users.noreply.github.com>
2023-12-25 17:10:26 +0300

    Update InstallerDates.java

--- src/main/java/org/example/InstallerDates.java
+++ src/main/java/org/example/InstallerDates.java
@@ -1,12 +1,11 @@
 package org.example;
 
 import java.util.List;
-import java.util.Scanner;
+
 import java.util.logging.Logger;
 
 public class InstallerDates {
     private static final Logger LOGGER = Logger.getLogger(InstallerDates.class.getName());
-    private static final Scanner scan = new Scanner(System.in);
     private String day;
     private String month;
     private String year;
@@ -45,26 +44,7 @@ public class InstallerDates {
         this.hour = hour;
         this.installerName = installerName;
     }
-    public static void addDate(List<InstallerDates> installerDatesList, String namee) {
 
-        // Create a new Product with the provided information
-
-        LOGGER.info("\u001B[33m Enter New Date:\u001B[0m");
-        LOGGER.info("\u001B[33m Day:\u001B[0m");
-        String dayy = scan.nextLine();
-
-        LOGGER.info("\u001B[33m Month:\u001B[0m");
-        String monthh = scan.nextLine();
-        LOGGER.info("\u001B[33m Year:\u001B[0m");
-        String yearr = scan.nextLine();
-        LOGGER.info("\u001B[33m Hour:\u001B[0m");
-        String hourr = scan.nextLine();
-
-        InstallerDates newDate = new InstallerDates(dayy, monthh, yearr, hourr, namee);
-        // Add the new product to the productList
-        installerDatesList.add(newDate);
-        LOGGER.info("\u001B[32mDate Added successfully.\u001B[0m");
-    }
     public static void viewDates(List<InstallerDates> installerDatesList) {
         int x = 1;
 
