c35d6cdff307152daaa5861e25cf895169740fdd
danahamdanx <145384079+danahamdanx@users.noreply.github.com>
2023-12-25 17:11:28 +0300

    Update Customer.java

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,18 +1,14 @@
 package org.example;
 
-import car.database.CategoryList;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.logging.Logger;
-import java.io.PrintWriter;
+
 
 public class Customer extends User {
-    private static final PrintWriter writer = new PrintWriter(System.out, true);
     public static void println(String message) {print(message);}
-    private static void print(String output) {writer.println(output);}
-    private static final Scanner scan = new Scanner(System.in);
+    private static void print(String output) {LOGGER.info(output);}
     public static boolean isViewed() {
         return isViewed;
     }
@@ -20,13 +16,7 @@ public class Customer extends User {
 
     private static boolean isViewed=false;
 
-    private static boolean isExisted = false;
 
-    public static boolean isIsSent() {
-        return isSent;
-    }
-
-    public static boolean isSent =false;
 
     public static class Sss {
         private ArrayList<Order> orderList = new ArrayList<>();
@@ -70,13 +60,7 @@ public class Customer extends User {
             this.quantity=quantity;
         }
 
-        public Product getProduct() {
-            return product;
-        }
 
-        public int getQuantity() {
-            return quantity;
-        }
 
         public double getTotalPrice() {
             // Assuming the product's price is a String, parse it to double for calculation
@@ -85,34 +69,10 @@ public class Customer extends User {
             // Calculate total price
             return productPrice * quantity;
         }
-        public static void placeOrder(List<Product> product, int selectedProductIndex) {
-
-            if (selectedProductIndex >= 0 && selectedProductIndex < product.size()) {
-                Product selectedProduct = product.get(selectedProductIndex);
-
-                if (selectedProduct.isAvailability()) {
-                    LOGGER.info("\u001b[35mQuantity:\u001b[0m");
-                    int quantity = scan.nextInt();
-
-                   println("\u001b[35mYou have selected: \u001b[35m" + quantity + "\u001b[35m of \u001b[35m" +
-                            selectedProduct.getName() + " " + selectedProduct.getPrice() + "$" + "\u001b[0m");
 
 
-                    // Add the order to the customer's order list
-                    Customer.Order newOrder = new Customer.Order(selectedProduct, quantity);
-                    oL.addOrder(newOrder);
 
-                    LOGGER.info("\u001b[35mOrder Added Successfully To Cart \u001b[0m");
-
-                    // Send order confirmation email
-                    sendOrderConfirmationEmail(selectedProduct.getName(), quantity, newOrder.getTotalPrice());
-                } else {
-                    LOGGER.info("\u001b[34mThe Product Out Of Stock\u001b[0m");
-                }
-            }
-        }
-
-        public static void sendOrderConfirmationEmail(String productName, int quantity, double totalPrice) {
+        public void sendOrderConfirmationEmail(String productName, int quantity, double totalPrice) {
             String from= "lemarizeq@gmail.com";
             String to = "dana29454@gmail.com";
 
@@ -122,55 +82,11 @@ public class Customer extends User {
                     + "Quantity: " + quantity + "\n"
                     + "Total Price: " + totalPrice + "$"+ "\n\n"
                     + "We appreciate your business.";
-            isSent =true;
+
 
             EmailSender.sendEmail(from, to, subject, messageText);
         }
     }
 
-    public static void search(List<Product>productList) {
-        List<Product> product1;
-        product1 = CategoryList.getProduct();
 
-
-        LOGGER.info("\u001b[35mEnter the product name:\u001b[0m");
-        String pname = scan.nextLine();
-         isExisted = false;
-
-        for (int i = 1; i < productList.size(); i++) {
-            Product product = productList.get(i);
-            if (product.getName().equalsIgnoreCase(pname)) {
-                String productInfo = "\u001b[35m"+i + ". " + product.getType() + " , " + product.getName() +
-                        " , " + product.getDescription() + " , " + product.getImage() +
-                        " , " + product.getPrice() + " $ " + " , " + product.isAvailability()+"\u001b[0m";
-                println("\u001B[35m" + productInfo + RESET);
-
-                isExisted = true;
-
-                // Ask if the user wants to buy the product
-                LOGGER.info("\u001b[35mDo you want to buy it? (yes/no): \u001b[0m");
-                String buyDecision = scan.nextLine();
-
-                // Process the user's decision
-                switch (buyDecision.toLowerCase()) {
-                    case "yes":
-                        // Pass the index of the selected product to the PlaceOrder method
-                        Order.placeOrder(product1, i);
-                        break;
-
-                    case "no":
-                        LOGGER.info("\u001b[34mMaybe next time. Have a great day!\u001b[0m");
-                        break;
-
-                    default:
-                        LOGGER.info("\u001b[34mInvalid Input. Please try again\u001b[0m");
-                        // No need for a break here since it's the last case
-                }
-              // Break out of the loop since the product is found
-            }
-        }
-        if (!isExisted) {
-            LOGGER.info("\u001b[34mProduct not found. Please check the product name and try again.\u001b[0m");
-        }
-    }
 }
